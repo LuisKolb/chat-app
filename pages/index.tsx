22fa36cf629002e7a0481a26121b6dec19237f0c
LuisKolb <kolb.luis@gmail.com>
2022-08-27 00:15:41 +0200

    build: add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,12 @@ import ChatMessage from '../components/chat/message/ChatMessage'
 import { mockChatMessageProps } from '../components/chat/message/ChatMessage.mocks'
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout'
 import SidebarLayout from '../components/layouts/sidebar/SidebarLayout'
-import styles from '../styles/Home.module.css'
 import { NextPageWithLayout } from './page'
 
 const Home: NextPageWithLayout = () => {
     return (
-        <section className={styles.main}>
-            <h1 className={styles.title}>
+        <section className="">
+            <h1 className="">
                 Welcome to <a href="https://nextjs.org">Next.js!</a>
             </h1>
             <ChatMessage {...mockChatMessageProps.recieved} />
